a5692404e9c4bc7089d46a7ea8bd9735058a1a2b
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2025-01-08 04:45:01 +0300

    working on the music player ui

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Music Player App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -24,8 +25,36 @@ class MusicPlayerScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Music Player'),
       ),
-      body: Center(
-        child: Text('Welcome to the Music Player App!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/clad.jpg'),
+          SizedBox(height: 20),
+          Text(
+            'Song Title',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Text('Artist Name'),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(Icons.skip_previous),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.play_arrow),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.skip_next),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
