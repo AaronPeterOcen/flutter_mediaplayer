a47b539af45a1d4b20698d255fb755dd98341c2d
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2025-01-08 04:32:27 +0300

    getting the flutter app to run successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mediaplayer/home_page.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music Player App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MusicPlayerScreen(),
+    );
+  }
+}
+
+class MusicPlayerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Music Player'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Music Player App!'),
+      ),
+    );
+  }
 }
